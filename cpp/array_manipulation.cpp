a6ea3a52a620c9c02f767803dc36a2f730188ccd
Qiuwen <qlou@outlook.com>
2019-06-14 16:12:03 -0400

    first commit

--- cpp/array_manipulation.cpp
+++ cpp/array_manipulation.cpp
@@ -1,1 +1,16 @@
-include
+
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main(int argc, char *argv[]){
+	string str="This is a complete sentence.";
+	string str1 = "sssw";
+
+	str.insert(10, str1);
+
+    cout << str << endl;
+	// cout << "Hello World!" << endl;
+	return 0;
+}
